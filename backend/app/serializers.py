b68603ba4821d2bd80ab2576f022aa3668af08f5
ripgandolfini <ddaniel97685@gmail.com>
2025-12-30 19:45:55 +0100

    Delete backend directory

--- backend/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Idopont
-
-class IdopontSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Idopont
-        fields = '__all__'
